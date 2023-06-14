feat: fix PT normalization due to kmax

LIM_b7/VID_computation.py
@@ -7,7 +7,7 @@ class VID(LineObs):
         
             # VID - RELATED QUANTITES
             nT=2**22,
-            Ngal_max=500,
+            Ngal_max=100,
             subtract_VID_mean=True,
             linear_VID_bin=False,
             do_sigma_G = True,
@@ -113,7 +113,7 @@ class VID(LineObs):
             kpar = ki*mui
             kperp = ki*np.sqrt(1.-mui**2.)
             
-            #Gaussian window for voxel -> FT
+            #Gaussian window for voxel -> FT    
             Wkpar2 = np.exp(-((kpar*self.sigma_par)**2).decompose())
             Wkperp2 = np.exp(-((kperp*self.sigma_perp)**2).decompose())
             Wk2 = Wkpar2*Wkperp2

LIM_b7/modelling/cosmo_astro_prescriptions_pmf.py
@@ -427,6 +427,7 @@ class LineModel(object):
                 else self.CDS_alpha_3 if self.k_23 <= k < self.k_34 \
                 else self.CDS_alpha_4
 
+            #output =  np.genfromtxt('pk_fid.txt')[:2000,1]
             output = Pk0 * np.vectorize(alpha)(k) * self.Dgrowth(z)**2
 
         elif self.developer == 'axions':
@@ -453,6 +454,7 @@ class LineModel(object):
         elif self.developer == 'PMF':
             # TalAdi: use kmax from dictionary
             # (should set the value in fiducial_pars accordingly)
+            
             kmax = self.kmax.value
             zmax = self.zcosmo[-1]
             nz_step = 64
@@ -496,7 +498,8 @@ class LineModel(object):
     @cached_property
     def Pm(self):
 
-        return self.PKint(self.z,self.ki_grid.value)*u.Mpc**3
+        #return self.PKint(self.z,self.ki_grid.value)*u.Mpc**3
+        return self.PKint(self.z,self.ki_grid)*u.Mpc**3
 
 
     # interpolate effective f depending on k,z + include tiling to multply by mu 
@@ -594,8 +597,11 @@ class LineModel(object):
     @cached_property
     def k(self):
 
+        #k_val = np.genfromtxt('pk_fid.txt')[:2000,0]
+        
         Nedge = self.k_edge.size
         
+        #return k_val 
         return (self.k_edge[0:Nedge-1]+self.k_edge[1:Nedge])/2.
     
     # width of k bins